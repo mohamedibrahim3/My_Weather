6d3d0ab01c68af56e56b9e3949e1f6ec52345a7d
mada <mohamedibra3232@gmail.com>
2025-06-09 16:08:01 +0300

    refactor weather use case and add location repository in it

--- app/src/main/java/com/example/myweather/di/useCaseModule.kt
+++ app/src/main/java/com/example/myweather/di/useCaseModule.kt
@@ -1,10 +1,8 @@
 package com.example.myweather.di
 
-import com.example.myweather.logic.usecases.GetCoordinateByLocationUseCase
 import com.example.myweather.logic.usecases.GetWeatherByCoordinateUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
-    single { GetCoordinateByLocationUseCase(get()) }
-    single { GetWeatherByCoordinateUseCase(get()) }
+    single { GetWeatherByCoordinateUseCase(get(),get()) }
 }
--- app/src/main/java/com/example/myweather/logic/usecases/GetWeatherByCoordinateUseCase.kt
+++ app/src/main/java/com/example/myweather/logic/usecases/GetWeatherByCoordinateUseCase.kt
@@ -1,11 +1,17 @@
 package com.example.myweather.logic.usecases
 
-import com.example.myweather.logic.model.LocationCoordinate
+import com.example.myweather.logic.model.Weather
+import com.example.myweather.logic.repository.LocationRepository
 import com.example.myweather.logic.repository.WeatherRepository
 
 class GetWeatherByCoordinateUseCase (
+    private val locationRepository: LocationRepository,
     private val weatherRepository: WeatherRepository
 ) {
-    suspend fun getWeatherByCoordinate(locationCoordinate: LocationCoordinate) =
-        weatherRepository.getWeatherByCoordinate(locationCoordinate)
+    suspend fun getWeatherByCoordinate(): Weather {
+        val location = locationRepository.getCurrentLocation()
+        val weather = weatherRepository.getWeatherByCoordinate(location)
+        return weather
+    }
+
 }
