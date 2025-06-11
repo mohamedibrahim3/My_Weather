388fa566da674b656651fdb953b75a5e3e010747
mada <mohamedibra3232@gmail.com>
2025-06-11 13:20:28 +0300

    add WeatherRepository interface to abstract weather data access

--- app/src/main/java/com/example/myweather/data/weather/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/myweather/data/weather/repository/WeatherRepositoryImpl.kt
@@ -1,19 +1,27 @@
 package com.example.myweather.data.weather.repository
 
-import com.example.myweather.data.weather.datasource.WeatherRemoteDataSource
-import com.example.myweather.data.weather.mapper.WeatherMapper
-import com.example.myweather.logic.model.LocationCoordinate
+import com.example.myweather.common.DataState
+import com.example.myweather.data.weather.datasource.WeatherDataSource
+import com.example.myweather.data.weather.mapper.toWeather
 import com.example.myweather.logic.model.Weather
 import com.example.myweather.logic.repository.WeatherRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class WeatherRepositoryImpl(
-    private val remoteDataSource: WeatherRemoteDataSource,
-    private val weatherMapper: WeatherMapper,
+    private val weatherDataSource: WeatherDataSource
 ) : WeatherRepository {
-
-
-    override suspend fun getWeatherByCoordinate(locationCoordinate: LocationCoordinate): Weather {
-        val weatherCondition = remoteDataSource.getDailyWeatherByCoordinate(locationCoordinate)
-        return weatherMapper.mapWeatherDtoToWeatherEntity(weatherCondition)
+    override suspend fun getWeatherByCoordinate(
+        lat: Double,
+        long: Double
+    ): Flow<DataState<Weather>> {
+        return weatherDataSource.getDailyWeatherByCoordinate(lat, long).map { dataState ->
+            when (dataState) {
+                is DataState.Loading -> DataState.Loading
+                is DataState.Success -> DataState.Success(dataState.data.toWeather())
+                is DataState.Error -> DataState.Error(dataState.message)
+            }
+        }
     }
+
 }
--- app/src/main/java/com/example/myweather/logic/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/myweather/logic/repository/WeatherRepository.kt
@@ -1,8 +1,9 @@
 package com.example.myweather.logic.repository
 
-import com.example.myweather.logic.model.LocationCoordinate
+import com.example.myweather.common.DataState
 import com.example.myweather.logic.model.Weather
+import kotlinx.coroutines.flow.Flow
 
 interface WeatherRepository {
-    suspend fun getWeatherByCoordinate(locationCoordinate: LocationCoordinate): Weather
+    suspend fun getWeatherByCoordinate(lat: Double, long: Double): Flow<DataState<Weather>>
 }
