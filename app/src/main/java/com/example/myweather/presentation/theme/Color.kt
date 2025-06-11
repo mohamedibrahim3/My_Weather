e14f157963cac6e4523a7934459cbfcd6ad9b472
mada <mohamedibra3232@gmail.com>
2025-06-11 13:05:24 +0300

    added app color palette

---
+++ app/src/main/java/com/example/myweather/presentation/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.myweather.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val PrimaryTextDark = Color(0xFFFFFFFF)
+val SecondaryTextDark = Color(0xDEFFFFFF)
+val TertiaryTextDark = Color(0x99FFFFFF)
+
+val PrimaryTextLight = Color(0xFF060414)
+val SecondaryTextLight = Color(0xFF323232)
+val TertiaryTextLight = Color(0x99060414)
