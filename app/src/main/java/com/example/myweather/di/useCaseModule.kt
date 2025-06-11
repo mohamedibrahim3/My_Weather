70582c2cbff32ed2c7a4e84ec05553cb30cb8e99
mada <mohamedibra3232@gmail.com>
2025-06-11 13:14:39 +0300

    add use case module

--- app/src/main/java/com/example/myweather/di/useCaseModule.kt
+++ app/src/main/java/com/example/myweather/di/useCaseModule.kt
@@ -1,8 +1,9 @@
 package com.example.myweather.di
 
 import com.example.myweather.logic.usecases.GetWeatherByCoordinateUseCase
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val useCaseModule = module {
-    single { GetWeatherByCoordinateUseCase(get(),get()) }
+    singleOf(::GetWeatherByCoordinateUseCase)
 }
